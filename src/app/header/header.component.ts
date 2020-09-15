4fac15ad43e92b867e3ab69c3a55b0bc7c167f03
KalyanG7 <kalyanasundaram_g@rediffmail.com>
2020-09-15 16:01:28 +0530

    Overallbuild

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,6 +9,7 @@ export class HeaderComponent implements OnInit {
 
   imgSrc="../assets/images/header1.jpg"
   titleTxt = "Indian School of Yoga"
+  subTitleTxt = "(An Institute of Yoga Research,Treatement & Training)"
   authorName = "Yoga Rathna Dr.A.S.Ashok Kumar, Ph.D.,(Y.Sc)"
 
   constructor() { }
